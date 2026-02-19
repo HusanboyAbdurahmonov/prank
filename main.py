99c49b1a0bf55b135923c8b41ddd8c89c6957312
HusanboyAbdurahmonov <abdurahmonovh309@gmail.com>
2026-02-19 17:24:11 +0500

    update design

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import requests
+import subprocess
+import os
 
-BOT_TOKEN = "8538918756:AAFWzZBWeTqgW0OszgOo-GNCihcYn0bv2FA"
-CHAT_ID = "8412394779"
+BOT_TOKEN = os.getenv("8538918756:AAFWzZBWeTqgW0OszgOo-GNCihcYn0bv2FA")
+CHAT_ID = os.getenv("8412394779")
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -17,19 +19,36 @@ async def get_form(request: Request):
 async def upload_video(file: UploadFile = File(...)):
     contents = await file.read()
 
+    # webm saqlash
+    with open("input.webm", "wb") as f:
+        f.write(contents)
+
+    # MP4 ga konvert (iPhone compatible)
+    subprocess.run([
+        "ffmpeg", "-i", "input.webm",
+        "-vcodec", "libx264",
+        "-profile:v", "baseline",
+        "-level", "3.0",
+        "-pix_fmt", "yuv420p",
+        "-movflags", "+faststart",
+        "-acodec", "aac",
+        "output.mp4"
+    ])
+
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendVideo"
 
-    files = {
-        "video": ("video.mp4", contents, "video/mp4")
-    }
+    with open("output.mp4", "rb") as video:
+        files = {
+            "video": ("video.mp4", video, "video/mp4")
+        }
 
-    data = {
-        "chat_id": CHAT_ID,
-        "supports_streaming": True
-    }
+        data = {
+            "chat_id": CHAT_ID,
+            "supports_streaming": True
+        }
 
-    response = requests.post(url, data=data, files=files)
+        response = requests.post(url, data=data, files=files)
+
     print(response.text)
 
     return {"status": "ok"}
-
