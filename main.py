670ab61c36f56ed1108972b5bafddac558424520
HusanboyAbdurahmonov <abdurahmonovh309@gmail.com>
2026-02-19 17:02:54 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import requests
+
+BOT_TOKEN = "8538918756:AAFWzZBWeTqgW0OszgOo-GNCihcYn0bv2FA"
+CHAT_ID = "8412394779"
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload")
+async def upload_video(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendVideo"
+
+    files = {
+        "video": ("video.mp4", contents, "video/mp4")
+    }
+
+    data = {
+        "chat_id": CHAT_ID,
+        "supports_streaming": True
+    }
+
+    response = requests.post(url, data=data, files=files)
+    print(response.text)
+
+    return {"status": "ok"}
+
